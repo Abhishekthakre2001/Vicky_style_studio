348f6201249108759954cc5523a7df37b45a0849
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2024-06-14 22:03:51 +0530

    all set

---
+++ js/appointment.js
@@ -0,0 +1,15 @@
+function bookappointment(){
+    let number ="7796556125";
+
+    let name = document.getElementById("name").value;
+    let time = document.getElementById("time").value;
+    let service = document.getElementById("service").value;
+    let date = document.getElementById("date").value;
+
+    // var url = "https://wa.me/"+number+"?text="+"Name:"+ name+"%0a"+"Time:"+time+"%0a"+"Service:"+service+"%0a"+"Date:"+date+"%0a";
+    let url = `https://wa.me/${number}?text=${encodeURIComponent(`Name: ${name}\nTime: ${time}\nService: ${service}\nDate: ${date}`)}`;
+    // window.open(url, '_blank').focus();
+
+    console.log(url);
+
+}
